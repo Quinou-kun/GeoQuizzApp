1b5d752450f2288c69f44c50c3d764f088b2c8d4
root <luc.andre4@etu.univ-lorraine.fr>
2018-02-05 18:09:47 +0100

    map & methods

---
+++ src/components/New.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-button to="/game"><h1>Start game ! </h1></b-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'This is the register page'
+    }
+  },
+  methods: {
+    register () {
+      console.log('register')
+    }
+  }
+}
+</script>
