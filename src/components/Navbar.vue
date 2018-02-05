85142cc04c644ef2af91bf76897c80033aead69c
Quentin Delamarre <quentin.delamarre8@etu.univ-lorraine.fr>
2018-02-05 17:20:37 +0100

    Fix active buttons in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,13 @@
   <div>
       <b-navbar class="geoquizz-navbar" toggleable="md" type="dark" variant="info">
           <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-          <b-navbar-brand href="#">GEOQUIZZ</b-navbar-brand>
+          <b-navbar-brand href="/">GEOQUIZZ</b-navbar-brand>
           <b-collapse is-nav id="nav_collapse">
               <!-- Right aligned nav items -->
               <b-navbar-nav class="ml-auto mr-5">
-                  <b-nav-item to="/" class="mr-3">Home</b-nav-item>
-                  <b-nav-item to="/new" class="mr-3">New Game</b-nav-item>
-                  <b-nav-item to="/signin" class="mr-3">Log In</b-nav-item>
+                  <b-nav-item to="/" exact class="mr-3">Home</b-nav-item>
+                  <b-nav-item to="/new" exact class="mr-3">New Game</b-nav-item>
+                  <b-nav-item to="/signin" exact class="mr-3">Log In</b-nav-item>
               </b-navbar-nav>
           </b-collapse>
       </b-navbar>
