c4088c2d23392b7f622ce7fe78a6f391d570b222
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2018-02-05 19:18:59 +0100

    Do timer to the map and stoping timer, display marker

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
-=======
-        <h1>{{timer}}</h1>
         <h2>Are you ready ? Click to the button below if you think you are !</h2>
->>>>>>> fd3a70b1241758a46271b9e8dc973c974acea504
         <b-button to="/game"><h1>Start game ! </h1></b-button>
     </div>
 </template>
@@ -14,42 +10,12 @@ export default {
   name: 'Register',
   data () {
     return {
-<<<<<<< HEAD
       msg: 'This is the register page'
     }
   },
   methods: {
     register () {
       console.log('register')
-=======
-      timer: 5,
-      msg: 'This is the register page'
-    }
-  },
-  created () {
-    this.count()
-  },
-  methods: {
-    register () {
-      console.log('register')
-    },
-    setTimer (sec) {
-      this.timer = sec
-    },
-    count () {
-      let interval = setInterval(() => {
-        this.setTimer(this.timer)
-        this.timer--
-        if (this.timer < 0) {
-          clearInterval(interval)
-          this.reset()
-        }
-      }, 1000)
-    },
-    reset () {
-      this.timer = 5
-      this.count()
->>>>>>> fd3a70b1241758a46271b9e8dc973c974acea504
     }
   }
 }
