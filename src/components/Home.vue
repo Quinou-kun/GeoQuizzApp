2e94b5355812a28b6ae07cb45683218d0e3fe92a
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2018-02-05 14:43:36 +0100

    Do routes for home and signin and then create form for sign in

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <div class="content">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'This is the home page'
+    }
+  },
+  components: {
+    'navbar': Navbar
+  }
+}
+</script>
+
+<style>
+    .geoquizz-nav{
+        padding : 50px 5px;
+        background-color : grey;
+    }
+    .geoquizz-nav a{
+        color : black;
+    }
+
+</style>
---
+++ src/components/Signin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-container>
+            <b-form @submit="signin()">
+                <b-form-group label="Email address :" label-for="exampleInput1" description="We'll never share your email with anyone else.">
+                    <b-form-input pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" id="exampleInput1" type="email" required placeholder="ex: john.doe@gmail.com"></b-form-input>
+                </b-form-group>
+                <b-form-group label="Password :" label-for="exampleInput2" description="Minimum eight characters, at least one letter and one number">
+                    <b-form-input pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" id="exampleInput2" type="password" required placeholder="*******">
+                    </b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      msg: 'This is the sign in page'
+    }
+  },
+  methods: {
+    signin () {
+      console.log('sign in')
+    }
+  }
+}
+</script>
