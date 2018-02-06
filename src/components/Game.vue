d2796f8967fffe50d8b27f995028c66272e222a4
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2018-02-06 10:10:42 +0100

    Do scoring on game with timer and distance

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -9,17 +9,18 @@
             <b-img :src="img" fluid alt="Responsive image"></b-img>
         </div>
         <div id="geo-map">
-        <v-map ref="map" id="map" :zoom=13 :center="[47.413220, -1.219482]" :zoomControl=false :options="option" @l-click="placeMarker">
+        <v-map ref="map" id="map" :zoom=15 :center="[48.6915784, 6.1767092]" :zoomControl=false :options="option" @l-click="placeMarker">
     		<v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
         </v-map>
         </div>
 
         <div>
-            <b-button @click="stopTimer()">This is here !</b-button>
+            <b-button v-if="!this.stop" @click="stopTimer()">This is here !</b-button>
         </div>
 
         <div v-if="this.stop">
-            <h1>+3pts</h1>
+            <h1>+ {{pts}} pts</h1>
+            <h1>Score : {{score}}</h1>
             <h3>Distance : {{distance}} meters</h3>
             <b-button>Go to the next !</b-button>
         </div>
@@ -49,17 +50,19 @@ export default {
     return {
       msg: 'This is the game page',
       zoom:13,
-      center: L.latLng(47.413220, -1.219482),
+      center: L.latLng(48.6915784, 6.1767092),
       url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       marker: null,
       option: {zoomControl : false, touchZoom : false, doubleClickZoom : false, scrollWheelZoom : false, boxZoom : false, keyboard : false},
       clicked : false,
       clickedMarker : null,
-      timer: 10,
+      timer: 30,
       img: 'http://lorempixel.com/500/500',
       stop: false,
-      distance: null
+      distance: null,
+      score: 0,
+      pts:0
     }
   },
   methods :{
@@ -67,14 +70,12 @@ export default {
   		if (!this.clicked){
 
 
-            this.marker = L.marker(L.latLng(47.413220, -1.219482))
+            this.marker = L.marker(L.latLng(48.6915784, 6.1767092))
 
   			this.clicked = true
 
   			this.clickedMarker = L.marker(event.latlng)
   			this.clickedMarker.addTo(this.$refs.map.mapObject)
-
-            this.distance = Math.round(10* this.clickedMarker.getLatLng().distanceTo(this.marker.getLatLng()))/10
   		}else{
   			this.clickedMarker.setLatLng(event.latlng)
   		}
@@ -88,16 +89,30 @@ export default {
         this.timer--
         if (this.timer < 0 || this.stop) {
           clearInterval(interval)
-          this.resetTimer()
           this.displaySolution()
         }
       }, 1000)
     },
     displaySolution(){
       this.marker.addTo(this.$refs.map.mapObject)
+      this.distance = Math.round(10* this.clickedMarker.getLatLng().distanceTo(this.marker.getLatLng()))/10
+      if(this.distance < 150){
+        this.pts += 5
+      } else if(this.distance < 300) {
+        this.pts += 3
+      } else{
+        this.pts += 1
+      }
+      if(this.timer >= 20){
+        this.pts*=4
+      } else if(this.timer >= 10){
+        this.pts*=2
+      }
+      this.score += this.pts
+      this.resetTimer()
     },
     resetTimer () {
-      this.timer = 10
+      this.timer = 30
     },
     stopTimer () {
   	  this.stop = !this.stop
@@ -112,8 +127,8 @@ export default {
 @import "../../node_modules/leaflet/dist/leaflet.css";
 
     #map{
-        width: 500px;
-        height: 500px;
+        width: 70vw;
+        height: 80vh;
     }
     #geo-map, #img{
         vertical-align : top;
